f77e37c5b6a9d994575092fea4d84401e89dfd9d
Marcos Almeida <x00143891189@tcu.gov.br>
2018-07-23 18:26:20 -0300

    Lição 69 - Call, Bind e Apply

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
-var years = [1990, 1965, 1937, 2005, 1998];
+var marcos = {
+    nome: 'Marcos',
+    idade: 33,
+    job: 'UX Designer',
+    apresentacao: function (estilo, tempo) {
+        if (estilo === 'formal') {
+            console.log ('Good ' + tempo + '! Eu sou o ' + this.nome + ', tenho ' + this.idade + ' anos e sou ' + this.job + '.')
+        } else {
+            console.log('What\' up! Good ' + tempo + ', eu sou o ' + this.nome + ', tenho ' + this.idade + ' anos e sou ' + this.job + '.')
+        }
+    }
+};
+
+var eliton = {
+    nome: 'Éliton',
+    idade: 30,
+    job: 'UI Designer',
+};
+
+
+//O Call utiliza o método de um outro objeto naquele em que você indicar.
+//Na prática o que ocorre dentro do método é a substituição do THIS. pelos dados do novo objeto.
+
+marcos.apresentacao.call(eliton, 'casual', 'afternoon');
+
+marcos.apresentacao('formal', 'morning');
+
+
+//O Bind cria uma nova função baseada em novos parametros que podem ser redefidos. Por exemplo, foram pré-definidos o THIS e o estilo.
+var elitonFormal = marcos.apresentacao.bind(eliton, 'formal');
+elitonFormal('night');
+
+
+var years = [1990, 1965,1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
     var arrRes = [];
-    for (let index = 0; index < arr.length; index++) {
-        arrRes.push(fn(arr[index]));
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
     }
     return arrRes;
 }
 
-function calculateAge(el) {
-    return 2018 - el;
+function calculateAge (el) {
+    return 2016 - el;
 }
 
-function isFullAge(el) {
-    return el >= 18;
+function isFullAge(limit, el) {
+    return el >= limit;
 }
-
-function maxHeartRate (el) {
-    if (el >= 18 && el <=81) {
-        return Math.round(206.9 - (0.67 * el));
-    } else {
-        return -1;
-    }
-}
-
 
 var ages = arrayCalc(years, calculateAge);
-var fullAges = arrayCalc(ages, isFullAge);
-var rate = arrayCalc(ages, maxHeartRate)
 
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 
-console.log(ages);
-console.log(rate);
+console.log(years);
+console.log(fullJapan);
